1f087ff7eb3af8a22f88b9fdf7097a5e1b5c5f05
João Torres <rey.joao@hotmail.com>
2024-05-14 19:51:44 +0100

    updated

--- Classes/Data.h
+++ Classes/Data.h
@@ -8,22 +8,19 @@
 
 class Data {
 public:
-    Data(const std::string& s);
+    Data(const std::string &s);
+
     const std::unordered_map<int, std::pair<float, float>> &getNodes() const;
 
     const Graph<std::string> &getGraph() const;
 
-//    // void readExtraGraphsEdges(const std::string &filename);
-//     //void readRealWorldGraphsVertex(const std::string &filename);
-//     //void readRealWorldGraphsEdges(const std::string &filename);
-//     //void readNodesOhioRizzGyatt(const std::string &filename);
-// //     void readToyGraphs(const std::string &filename);
-//     void readToyGraphsTourism(const std::string &filename);
-//     void readToyGraphsEdges(const std::string &basicString);
 
     void readExtraGraphs(const std::string &filename);
+
     void readGraphs(const std::string &filename);
+
     void readNodes(const std::string &filename);
+
     void readToyGraphsTourism(const std::string &filename);
 
 
--- Classes/TspManager.cpp
+++ Classes/TspManager.cpp
@@ -15,7 +15,7 @@ TspManager::TspManager(const Data &d) {
 }
 
 void TspManager::TSPbacktracking() {
-    if(!graph.getVertexSet().empty()){
+    if (!graph.getVertexSet().empty()) {
         vector<int> bestTour;
         TSPbacktrackingMethod(bestTour);
         cout << "Best tour: ";
@@ -29,34 +29,35 @@ void TspManager::TSPbacktracking() {
     }
 }
 
-void TspManager::TSPbacktrackingMethod(vector<int>& bestTour) {
+void TspManager::TSPbacktrackingMethod(vector<int> &bestTour) {
     double minTourCost = INT_MAX;
     int startNode = 0;
     vector<int> tour = {startNode};
     vector<bool> visited(graph.getNumVertex(), false);
     visited[startNode] = true;
-    TSPRec(tour,visited,0.0, minTourCost,bestTour);
+    TSPRec(tour, visited, 0.0, minTourCost, bestTour);
 
 }
 
-void TspManager::TSPRec(vector<int>& tour, vector<bool>& visited, double currentCost, double& minCost, vector<int>& bestTour) {
+void TspManager::TSPRec(vector<int> &tour, vector<bool> &visited, double currentCost, double &minCost,
+                        vector<int> &bestTour) {
     if (tour.size() == graph.getNumVertex()) {
-        minCost = min(minCost,currentCost);
+        minCost = min(minCost, currentCost);
         if (currentCost == minCost) {
             bestTour = tour;
             return;
         }
     } else {
         for (int i = 0; i < graph.getNumVertex(); ++i) {
-            if (!visited[i]){
-               int lastNode = tour.back();//tour[tour.size()-1];
-               if(hasEdge(graph.findVertex(std::to_string(lastNode)),graph.findVertex(std::to_string(i)))){
-                   visited[i] = true;
-                   tour.push_back(i);
-                   TSPRec(tour,visited,currentCost + getEdgeWeight(graph,lastNode,i),minCost,bestTour);
-                   visited[i] = false;
-                   tour.pop_back();
-               }
+            if (!visited[i]) {
+                int lastNode = tour.back();//tour[tour.size()-1];
+                if (hasEdge(graph.findVertex(std::to_string(lastNode)), graph.findVertex(std::to_string(i)))) {
+                    visited[i] = true;
+                    tour.push_back(i);
+                    TSPRec(tour, visited, currentCost + getEdgeWeight(graph, lastNode, i), minCost, bestTour);
+                    visited[i] = false;
+                    tour.pop_back();
+                }
             }
         }
     }
@@ -64,7 +65,7 @@ void TspManager::TSPRec(vector<int>& tour, vector<bool>& visited, double current
 }
 
 bool TspManager::hasEdge(Vertex<string> *pVertex, Vertex<string> *pVertex1) {
-    for (auto edge : pVertex->getAdj()) {
+    for (auto edge: pVertex->getAdj()) {
         if (edge->getDest() == pVertex1) {
             return true;
         }
@@ -74,7 +75,7 @@ bool TspManager::hasEdge(Vertex<string> *pVertex, Vertex<string> *pVertex1) {
 }
 
 double TspManager::getEdgeWeight(Graph<std::string> graph, int node, int i) {
-    for (auto edge : graph.findVertex(std::to_string(node))->getAdj()) {
+    for (auto edge: graph.findVertex(std::to_string(node))->getAdj()) {
         if (edge->getDest()->getInfo() == std::to_string(i)) {
             return edge->getWeight();
         }
@@ -97,7 +98,7 @@ void TspManager::TSPtriangularHeuristic() {
     }
 }
 
-void TspManager::TSPtriangularHeuristicMethod(vector<int>& bestTour) {
+void TspManager::TSPtriangularHeuristicMethod(vector<int> &bestTour) {
     vector<int> tour;
     vector<bool> visited(graph.getNumVertex(), false);
     int startNode = 0;
@@ -127,15 +128,16 @@ void TspManager::TSPtriangularHeuristicMethod(vector<int>& bestTour) {
     }
     bestTour = tour;
 }
-template <class T>
+
+template<class T>
 struct CompareEdgeWeights {
-    bool operator()(const Edge<T>* e1, const Edge<T>* e2) const {
+    bool operator()(const Edge<T> *e1, const Edge<T> *e2) const {
         return e1->getWeight() > e2->getWeight();
     }
 };
 
 void TspManager::TSPprim() {
-    //CompleteGraph(graph);
+    CompleteGraph(graph);
     if (graph.getNumVertex() == 0) return;
     Vertex<std::string> *startVertex = graph.getVertexSet()[0];
 
@@ -178,96 +180,81 @@ void TspManager::TSPprim() {
     std::cout << "Total weight: " << totalWeight << std::endl;
 
 }
-float TspManager::getLatitude(Vertex<std::string>* vertex) const {
-    // Check if vertex exists in nodesloc map
+
+float TspManager::getLatitude(Vertex<std::string> *vertex) const {
     auto it = nodesloc.find(std::stoi(vertex->getInfo()));
     if (it != nodesloc.end()) {
-        // Return latitude of the vertex
         return it->second.first;
     }
-    // Return a default value (e.g., -1) if vertex not found
-    return -1.0f; // You can choose a suitable default value
+    return -1.0f;
 }
 
-// Function to get longitude of a vertex
-float TspManager::getLongitude(Vertex<std::string>* vertex) const {
-    // Check if vertex exists in nodesloc map
+
+float TspManager::getLongitude(Vertex<std::string> *vertex) const {
+
     auto it = nodesloc.find(std::stoi(vertex->getInfo()));
     if (it != nodesloc.end()) {
-        // Return longitude of the vertex
+
         return it->second.second;
     }
-    // Return a default value (e.g., -1) if vertex not found
-    return -1.0f; // You can choose a suitable default value
+
+    return -1.0f;
 }
 
 
-
-
-
-void TspManager::printNetworkInfo(const string& system){
+void TspManager::printNetworkInfo(const string &system) {
     if (system == "real1" || system == "real2" || system == "real3") {
-        for (auto node : nodesloc) {
+        for (auto node: nodesloc) {
             cout << "Vertex: " << node.first << " X: " << node.second.first << " Y: " << node.second.second << endl;
         }
-    }
-    else if (system == "tourism"){
-        for (const auto& label : labels) {
+    } else if (system == "tourism") {
+        for (const auto &label: labels) {
             cout << "Vertex: " << label.first << " Label: " << label.second << endl;
         }
-    }
-    else if (system == "stadiums" || system == "shipping") {
-        for (auto vertex : graph.getVertexSet()) {
+    } else if (system == "stadiums" || system == "shipping") {
+        for (auto vertex: graph.getVertexSet()) {
             cout << "Vertex: " << vertex->getInfo() << endl;
         }
-    }
-    else {
-        for (auto vertex : graph.getVertexSet()) {
+    } else {
+        for (auto vertex: graph.getVertexSet()) {
             auto node = nodesloc.find(stoi(vertex->getInfo()));
             if (node != nodesloc.end()) {
-                cout << "Vertex: " << vertex->getInfo() << " X: " << node->second.first << " Y: " << node->second.second << endl;
+                cout << "Vertex: " << vertex->getInfo() << " X: " << node->second.first << " Y: " << node->second.second
+                     << endl;
             }
         }
     }
 }
 
 void TspManager::CompleteGraph(Graph<std::string> graphcopy) {
-
-
-        std::vector<Vertex<std::string>*> vertices = graphcopy.getVertexSet();
-
-        // Iterate over all pairs of vertices
-        for (size_t i = 0; i < vertices.size(); ++i) {
-            for (size_t j = i + 1; j < vertices.size(); ++j) {
-                Vertex<std::string>* v1 = vertices[i];
-                Vertex<std::string>* v2 = vertices[j];
-                // Compute Haversine distance between v1 and v2
-                double distance = haversineDistance(getLatitude(v1), getLongitude(v1), getLatitude(v2), getLongitude(v2));
-
-
-                // Add edge from v1 to v2
-                graphcopy.addEdge(v1->getInfo(), v2->getInfo(), distance);
-                // Add edge from v2 to v1 (assuming undirected graph)
-                graphcopy.addEdge(v2->getInfo(), v1->getInfo(), distance);
-            }
+    std::vector<Vertex<std::string> *> vertices = graphcopy.getVertexSet();
+    for (size_t i = 0; i < vertices.size(); ++i) {
+        for (size_t j = i + 1; j < vertices.size(); ++j) {
+            Vertex<std::string> *v1 = vertices[i];
+            Vertex<std::string> *v2 = vertices[j];
+            double distance = haversineDistance(getLatitude(v1), getLongitude(v1), getLatitude(v2), getLongitude(v2));
+            graphcopy.addEdge(v1->getInfo(), v2->getInfo(), distance);
+            graphcopy.addEdge(v2->getInfo(), v1->getInfo(), distance);
         }
     }
-    double TspManager::haversineDistance(double lat1, double lon1, double lat2, double lon2) {
-        // Convert latitude and longitude from degrees to radians
-        double lat1Rad = lat1 * M_PI / 180.0;
-        double lon1Rad = lon1 * M_PI / 180.0;
-        double lat2Rad = lat2 * M_PI / 180.0;
-        double lon2Rad = lon2 * M_PI / 180.0;
+}
 
-        // Haversine formula
-        double dLat = lat2Rad - lat1Rad;
-        double dLon = lon2Rad - lon1Rad;
-        double a = sin(dLat / 2) * sin(dLat / 2) +
-                   cos(lat1Rad) * cos(lat2Rad) *
-                   sin(dLon / 2) * sin(dLon / 2);
-        double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-        double distance = 6371 * c; // Earth's radius in kilometers
+double TspManager::haversineDistance(double lat1, double lon1, double lat2, double lon2) {
 
-        return distance;
-    }
+    double lat1Rad = lat1 * M_PI / 180.0;
+    double lon1Rad = lon1 * M_PI / 180.0;
+    double lat2Rad = lat2 * M_PI / 180.0;
+    double lon2Rad = lon2 * M_PI / 180.0;
+
+
+    double dLat = lat2Rad - lat1Rad;
+    double dLon = lon2Rad - lon1Rad;
+    double a = sin(dLat / 2) * sin(dLat / 2) +
+               cos(lat1Rad) * cos(lat2Rad) *
+               sin(dLon / 2) * sin(dLon / 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    double distance = 6371 * c;
+
+    return distance;
+}
 
